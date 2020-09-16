e933d4fdfe13503eebd87527b9373a0765d12610
Lucian Dumitru <lucian.dumitru@gmail.com>
2020-09-16 11:24:19 +0300

    Solve the last Broken Link + Update la Docusaurus v2.0.0-alpha.64

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
     plugins: [
      require.resolve('@cmfcmf/docusaurus-search-local')
     ],
-    onBrokenLinks: 'ignore',
+//    onBrokenLinks: 'ignore',
     title: 'Invăţarea uşoară a chimiei',
     tagline: 'Invăţăm prin experimente',
     url: 'https://lucidumitru.github.io',
